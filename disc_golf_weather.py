930e74eda0dc05c4f16e34254559e16fbc3b5d70
Dan <danielwindheim@gmail.com>
2018-04-05 11:32:41 -0700

    Update disc_golf_weather.py
    added pull_json helper function, darksky support, google maps geocoding for darksky

--- disc_golf_weather.py
+++ disc_golf_weather.py
@@ -5,8 +5,10 @@ import requests
 
 OWM_KEY = '' # Sign up for a free API key at http://openweathermap.org/appid
 WUG_KEY = '' # Sign up for a free API key at https://www.wunderground.com/weather/api/
+GGL_KEY = '' # Sign up for a free API key at https://developers.google.com/maps/documentation/geocoding/start
+DSN_KEY = '' # Sign up for a free API key at https://darksky.net/dev/docs
 
-# Locations with city codes for openweathermap.org. Wunderground doesn't require city codes.
+# Locations with city codes for openweathermap.org. Wunderground and DarkSky doesn't require city codes.
 
 LOCATIONS = {'Eugene' : '5725846', \
             'Dexter': '5735869', \
@@ -30,7 +32,7 @@ def main():
     print("*" * 40)
     while True:
         print("\nAt which website would you like to check the weather?\n\n")
-        answer_one = input("1: OpenWeatherMap.org -- Short Forecast with High/Low \n\n2: Wunderground.com -- Verbose Forecast\n\n")
+        answer_one = input("1: OpenWeatherMap.org -- Short Forecast with High/Low \n\n2: Wunderground.com -- Verbose Forecast\n\n3: DarkSky.net -- Weekly Summary\n\n")
         if answer_one == '1':
             get_openweather(LOCATIONS)
             answer_two = input('Would you like to check areas outside of Lane County? Y/n\n\n')
@@ -51,18 +53,30 @@ def main():
             else:
                 print("\nYeah I wouldn't want to drive out there either. Have fun huckin'!\n")
                 return False
+        elif answer_one == '3':
+            get_darksky(LOCATIONS)
+            answer_two = input('Would you like to check areas outside of Lane County? Y/n\n\n')
+            if answer_two.lower() == "y":
+                get_darksky(EXTRA_LOCATIONS)
+                print("\nSick.\n")
+                return False
+            else:
+                print("\nNo judgment here. Have a huckin' good time!\n")
+                return False
         else:
             print("That's not a valid selection. Let's try again.")
+
+def pull_json(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    json_data = json.loads(response.text)
+    return json_data
 
 def get_openweather(location):
     for place, city_code in location.items():
         url = 'http://api.openweathermap.org/data/2.5/forecast?id={id}&appid={key}&cnt=3&units=imperial'.format(id=city_code, key=OWM_KEY)
 
-        response = requests.get(url)
-        response.raise_for_status()
-        weather_data = json.loads(response.text)
-
-        w = weather_data['list']
+        w = pull_json(url)['list']
 
         todays_high = str(round((w[0]['main']['temp_max'])))
         todays_low = str(round((w[0]['main']['temp_min'])))
@@ -74,12 +88,10 @@ def get_openweather(location):
         print('*' * 40)
         print('Today\'s weather in {place}:'.format(place=place))
         print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
-        print('High of ' + todays_high + 'F, low of ' + todays_low + 'F')
-        print()
+        print('High of ' + todays_high + 'F, low of ' + todays_low + 'F\n')
         print('Tomorrow in {place}:'.format(place=place))
         print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
-        print('High of ' + tomorrows_high + 'F, low of ' + tomorrows_low + 'F')
-        print()
+        print('High of ' + tomorrows_high + 'F, low of ' + tomorrows_low + 'F\n')
         print('Day after tomorrow in {place}:'.format(place=place))
         print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
         print('High of ' + day_after_tomorrows_high + 'F, low of ' + day_after_tomorrows_low + 'F')
@@ -87,24 +99,42 @@ def get_openweather(location):
 
 def get_wunderground(location):
     for place, city_code in location.items():
-        url = "http://api.wunderground.com/api/{key}/forecast/q/OR/{city}.json".format(key=WUG_KEY, city=place)
-        
-        response = requests.get(url)
-        response.raise_for_status()
-        weather_data = json.loads(response.text)
+        url = "http://api.wunderground.com/api/{key}/forecast/q/{state}/{city}.json".format(key=WUG_KEY, state=STATE, city=place)
 
-        w = weather_data['forecast']['txt_forecast']['forecastday']
+        w = pull_json(url)['forecast']['txt_forecast']['forecastday']
 
-        print('*' * 140)
+        print('*' * 100)
         print('Today\'s weather in {place}:'.format(place=place))
-        print(w[0]['fcttext'])
-        print()
+        print(w[0]['fcttext'], '\n')
         print('Tomorrow in {place}:'.format(place=place))
-        print(w[1]['fcttext'])
-        print()
+        print(w[1]['fcttext'], '\n')
         print('Tomorrow in {place}:'.format(place=place))
         print(w[2]['fcttext'])
-        print('*' * 140)
+        print('*' * 100)
+
+def gmaps_geolocator(location):
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address={place},+{state}&key={key}".format(place=location, state=STATE, key=GGL_KEY)
+
+    geometry_data = pull_json(url)
+
+    place_latitude = geometry_data['results'][0]['geometry']['location']['lat']
+    place_longitude = geometry_data['results'][0]['geometry']['location']['lng']
+    return place_latitude, place_longitude
+
+def get_darksky(location):
+    print("Powered by Dark Sky: https://darksky.net/poweredby")
+    for place, city_code in location.items():
+        lat, lng = gmaps_geolocator(place)
+        url = "https://api.darksky.net/forecast/{key}/{lat},{lng}".format(key=DSN_KEY, lat=str(lat), lng=str(lng))
+
+        w = pull_json(url)
+
+        print('*' * 100)
+        print('Today\'s weather in {place}\n'.format(place=place))
+        print(w['daily']['summary'])
+        print('*' * 100)
+
+
 
 if __name__ == "__main__":
     main()
